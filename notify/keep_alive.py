33a2a86692ef9787a9da925d98a3416c23b3135b
poowis121 <>
2021-07-06 17:36:10 +0000

    add check endpoint

--- notify/keep_alive.py
+++ notify/keep_alive.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from flask import request
 from threading import Thread
+from check_item import is_item_in_stock
 
 app = Flask('')
 
 @app.route('/')
 def main():
   return "Bot Is Alive"
+
+@app.route('/check')
+def check():
+  url = request.args.get('url')
+  tag = request.args.get('tag')
+  attr_key = request.args.get('attr_key')
+  attr_val = request.args.get('attr_val')
+  sold_out_label = request.args.get('sold_out_label')
+
+  return str(is_item_in_stock(url, tag, attr_key, attr_val, sold_out_label))
 
 def run():
   app.run(host="0.0.0.0", port=8000)
